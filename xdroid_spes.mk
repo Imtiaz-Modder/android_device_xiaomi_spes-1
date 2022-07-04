bb3928b1fa820a3e56a4d99b7078b5f495d20b43
Imtiaz Modder <67188507+Imtiaz-Modder@users.noreply.github.com>
2022-07-04 06:04:06 +0600

    Update and rename lineage_spes.mk to xdroid_spes.mk

--- xdroid_spes.mk
+++ xdroid_spes.mk
@@ -12,7 +12,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/spes/device.mk)
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/xdroid/config/common_full_phone.mk)
 
 # Additions
 $(call inherit-product-if-exists, vendor/gapps/basic/config.mk)
@@ -21,7 +21,7 @@ $(call inherit-product-if-exists, vendor/gapps/basic/config.mk)
 $(call inherit-product-if-exists, vendor/miuicamera/config.mk)
 
 # Product Specifics
-PRODUCT_NAME := lineage_spes
+PRODUCT_NAME := xdroid_spes
 PRODUCT_DEVICE := spes
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := Redmi Note 11
@@ -40,3 +40,11 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="spes-user 11 RKQ1.211001.001 V13.0.9.0.RGCMIXM release-keys"
 
 BUILD_FINGERPRINT := Redmi/spes/spes:11/RKQ1.211001.001/V13.0.9.0.RGCMIXM:user/release-keys
+TARGET_SUPPORTS_QUICK_TAP := true
+
+TARGET_FACE_UNLOCK_SUPPORTED := true
+
+TARGET_SUPPORTS_BLUR := true/false
+
+WITH_GAPPS := true
+
